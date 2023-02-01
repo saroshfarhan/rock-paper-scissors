90fcb01d4b08b431aee54a2635f2399355da974f
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-02-01 23:49:47 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-semibold text-red-400">
+        Rock paper scissors
+      </h1>
+    </div>
+  );
+}
+
+export default App;
