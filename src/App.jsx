05ee7d71dc24a85345f9b5c465e5d0dc6b37ae68
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-02-02 00:50:03 +0530

    score container completed for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import React from "react";
+import Game from "./conponents/game";
+
 function App() {
   return (
-    <div className="container">
-      <h1 className="text-center text-3xl font-semibold text-scoreText">
-        Rock paper scissors
-      </h1>
+    <div className="container mx-auto">
+      <div
+        id="score-container"
+        className="mx-32 mt-10 flex items-center justify-between rounded-xl border border-white px-6 py-4"
+      >
+        <section className="text-white">
+          <h1 className="text-4xl font-extrabold uppercase">Rock</h1>
+          <h1 className="text-4xl font-extrabold uppercase">Paper</h1>
+          <h1 className="text-4xl font-extrabold uppercase">Scissors</h1>
+        </section>
+        <div className="grid h-[7.2rem] w-[8rem] place-content-center gap-0 rounded-lg bg-white text-center text-scoreText">
+          <p className="text-sm font-semibold uppercase">Score</p>
+          <h1 className="text-[3rem] font-bold">12</h1>
+        </div>
+      </div>
+      <Game />
     </div>
   );
 }
---
+++ src/conponents/game.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import triangleBg from "../assets/bg-triangle.svg";
+
+function Game() {
+  return (
+    <div id="game-container" className="mx-32 mt-10">
+      <h1 className="text-center text-3xl text-white">Game component</h1>
+    </div>
+  );
+}
+
+export default Game;
